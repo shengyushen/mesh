3fb45e0b51f6e211ed2662d03d9e421485b44580
shengyushen <shengyushen@aliyun.com>
2019-05-08 15:32:02 +0800

    capture according to the log file

--- capture.sh
+++ capture.sh
@@ -1,2 +1,3 @@
-capture_tpu_profile --tpu=ssy-mtf-ctpu  --logdir=gs://ssystore1/anothertransformer/output --num_tracing_attempts=10  --duration_ms=50000
+grep "checkpoints for 0 into" $1 | awk '{xx=substr($6,1,length($6)-1);n=split(xx,arr,"/");printf "capture_tpu_profile --tpu=ssy-mtf-ctpu  --logdir=" ; for(i=1;i<=n-2;i=i+1) {printf arr[i] "/"} ;print "output --num_tracing_attempts=10  --duration_ms=50000"}' > capture_gened.sh
+#capture_tpu_profile --tpu=ssy-mtf-ctpu  --logdir=gs://ssystore1/anothertransformer/output --num_tracing_attempts=10  --duration_ms=50000
 
--- tnsbrd.sh
+++ tnsbrd.sh
@@ -1,2 +1,3 @@
-tensorboard --logdir=gs://ssystore1/anothertransformer/output
+grep "checkpoints for 0 into" $1 | awk '{xx=substr($6,1,length($6)-1);n=split(xx,arr,"/");printf "tensorboard  --logdir=" ; for(i=1;i<=n-2;i=i+1) {printf arr[i] "/"} ;print "output"}' > gened_tnsbrd.sh
+#tensorboard --logdir=gs://ssystore1/anothertransformer/output
 
