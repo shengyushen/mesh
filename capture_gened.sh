8d2031c2a6776b9e850a9b3ae983d5ae5095684d
shengyushen <shengyushen@aliyun.com>
2019-05-11 03:28:22 +0000

    adding result for m4b8

--- capture_gened.sh
+++ capture_gened.sh
@@ -1,1 +1,1 @@
-capture_tpu_profile --tpu=ssy-mtf-ctpu  --logdir=gs://ssystore1/anothertransformer2019-05-08-07:47:48/output --num_tracing_attempts=10  --duration_ms=50000
+capture_tpu_profile --tpu=ssy-mtf-ctpu  --logdir=gs://ssystore1/anothertransformer2019-05-11-03:21:38/output --num_tracing_attempts=10  --duration_ms=50000
